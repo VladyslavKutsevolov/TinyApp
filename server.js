1d5ee23e06060400c919d4c157a324eb336c6795
Vladyslav <kytsevolov@gmail.com>
2020-08-31 17:14:50 -0600

    added template engine basic layout

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.render('');
 });
 
-app.get('/url.json', (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
