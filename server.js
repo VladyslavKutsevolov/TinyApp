c0a8b9ba2c984e3ad89b179f9fa7a198f617b5bb
Vladyslav <kytsevolov@gmail.com>
2020-08-31 19:45:20 -0600

    post and redirect

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { generateRandSring } = require('./utils');
+const { generateRandomString } = require('./utils');
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-const urlDatabase = {
+let urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
@@ -24,13 +24,22 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('ok');
+  const { longURL } = req.body;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const { shortURL } = req.params;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const { params } = req;
-  const templateVars = { shortURL: params.shortURL, longURL: params.longURL };
+  const { shortURL } = req.params;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL, longURL };
   res.render('urls_show', templateVars);
 });
 
